9cefaee5029110b0b111def6841fa3cb7363c22a
min <alsjd6702@gmail.com>
2024-09-26 16:54:22 +0900

    first commit

--- Planet2536_0823/js/main.js
+++ Planet2536_0823/js/main.js
@@ -1,29 +1,75 @@
-function logoopacity(){
-    logo.classList.remove("on");
-}
-window.onscroll = function(){
-    let ht = document.documentElement.scrollTop;
-    let logo = document.querySelector(".intro .logo img");
-    // console.log(ht);
-    if(ht > 350){
-        logo.classList.add("on");
-    } if(ht > 550){                
-        logo.classList.add("on2");
-    } if(ht > 750){
-        logo.classList.add("on3");
-    } if(ht > 2000){
-        logo.classList.add("on4");
-    }
+gsap.to(".intro_detail_position p:first-child",{
+    opacity: 1,
+    y: -30,
+    scrollTrigger: {
+        trigger: ".intro_detail_position p:first-child",
+        // markers: true,
+        start: "top 45%",
+        end: "bottom 50%",
+        toggleActions: "play pause resume reverse",
+        scrub: 0,
+    },
+});
+gsap.to(".intro_detail_position p:nth-child(2)",{
+    opacity: 1,
+    y: -30,
+    scrollTrigger: {
+        trigger: ".intro_detail_position p:nth-child(2)",
+        // markers: true,
+        start: "top 46%",
+        end: "bottom 51%",
+        toggleActions: "play pause resume reverse",
+        scrub: 0,
+    },
+});
+gsap.to(".intro_detail_position p:nth-child(3)",{
+    opacity: 1,
+    y: -30,
+    scrollTrigger: {
+        trigger: ".intro_detail_position p:nth-child(3)",
+        // markers: true,
+        start: "top 47%",
+        end: "bottom 52%",
+        toggleActions: "play pause resume reverse",
+        scrub: 0,
+    },
+});
+gsap.to(".intro_detail_position p:nth-child(4)",{
+    opacity: 1,
+    y: -30,
+    scrollTrigger: {
+        trigger: ".intro_detail_position p:nth-child(4)",
+        // markers: true,
+        start: "top 48%",
+        end: "bottom 53%",
+        toggleActions: "play pause resume reverse",
+        scrub: 0,
+    },
+});
+gsap.to(".intro_detail_position p:nth-child(5)",{
+    opacity: 1,
+    y: -30,
+    scrollTrigger: {
+        trigger: ".intro_detail_position p:nth-child(5)",
+        // markers: true,
+        start: "top 49%",
+        end: "bottom 54%",
+        toggleActions: "play pause resume reverse",
+        scrub: 0,
+    },
+});
 
-    let introCon = document.querySelectorAll(".intro_detail_position p");
-    if(ht > 700){
-        introCon[0].classList.add("on");     
-        introCon[1].classList.add("on");     
-        introCon[2].classList.add("on");     
-        introCon[3].classList.add("on");     
-        introCon[4].classList.add("on");
-    }
-}
+gsap.to(".logo",{
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".logo",
+        // markers: true,
+        start: "2000% 60%",
+        end: "6500% 20%",
+        toggleActions: "play pause resume reverse",
+        scrub: 2,
+    },
+});
 
 let classImg = document.querySelector(".class_img");
 let classTitle = document.querySelector(".class_txt");
