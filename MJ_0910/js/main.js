328698392da2324d49f4c9a9867c6648db2e845e
min <alsjd6702@gmail.com>
2024-10-08 11:04:16 +0900

    first commit

---
+++ MJ_0910/js/main.js
@@ -0,0 +1,14 @@
+var myFullpage = new fullpage('#fullpage', {
+
+    // you can also use the HTML attribute data-tooltip on each section instead
+    navigationTooltips: ['First Section', 'Second Section', 'Third Section'],
+
+    // Navigation for horizontal slides
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom', // Default
+
+    controlArrows: false,
+
+    // Get your license at https://alvarotrigo.com/fullPage/pricing
+    licenseKey: 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+});
