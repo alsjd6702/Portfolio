32ad6fb1b7f91918092ff004040e5a8f13755d6d
min <alsjd6702@gmail.com>
2024-10-11 12:52:34 +0900

    first commit

--- MJ_0910/js/main.js
+++ MJ_0910/js/main.js
@@ -3,10 +3,6 @@ $(document).ready(function() {
         verticalCentered: true,
         anchors: ['anchor1', 'anchor2', 'anchor3', 'anchor4', 'anchor5'],
         menu: '#menu',
-        sectionsColor: ['background-image: linear-gradient(to top, #4481eb 0%, #66a6ff 100%);', 'background-image: linear-gradient(-225deg, #7742B2 0%, #F180FF 52%, #FD8BD9 100%);', '#ccc', '#ccc', '#aaa']
+        sectionsColor: ['background-image: linear-gradient(to top, #4481eb 0%, #66a6ff 100%);', 'background-image: linear-gradient(-225deg, #7742B2 0%, #F180FF 52%, #FD8BD9 100%);', 'background-image: linear-gradient(45deg, #93a5cf 0%, #e9e9e7 50%, #e4efe9 100%);', 'background-image: linear-gradient(to top, #0ba360 0%, #3cba92 100%);', '#aaa']
     });
 });
-// #7BAABE
-// background-image: linear-gradient(to top, #3b41c5 0%, #a981bb 49%, #ffc8a9 100%);
-
-// background-image: linear-gradient(-225deg, #5271C4 0%, #B19FFF 48%, #ECA1FE 100%);
