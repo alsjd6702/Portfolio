b981caffb4dad5147abc07c66de8341150dc931f
min <alsjd6702@gmail.com>
2024-10-15 13:55:35 +0900

    first commit

--- High1_0911/js/main_rev3.js
+++ High1_0911/js/main_rev3.js
@@ -15,30 +15,37 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-let width = window.innerWidth;
-console.log(width);
 
-if(width <= 994){
-    document.querySelector(".topmenu").onclick = function(){
+document.querySelector(".topmenu").onclick = function(){
+    if(window.innerWidth <= 994){
         document.querySelector("header nav").style.left = "0";
+        document.querySelector("header nav > p").onclick = function(){
+            document.querySelector("header nav").style.left = "-450px";
+            if(window.innerWidth > 994){
+                document.querySelector("header nav").style.left = "0px";
+            }
+        };
     }
-
-    document.querySelector("header nav > p").onclick = function(){
-      document.querySelector("header nav").style.left = "-450px";
-    };
-} else if(width > 994){
-    document.querySelector("header nav").style.left = "0px";
 }
 
-  // $(function(){
-  //   $(".topmenu").click(function(){
-  //     $("header nav").css("left","0").css("transition","0.3s");
-  //   });
-  //   $("header nav > p").click(function(e){
-  //     e.preventDefault();
-  //     $("header nav").css("left","-450px");
-  //   });
-  // });
+// if(window.innerWidth <= 994){
+//     document.querySelector(".topmenu").onclick = function(){
+//         document.querySelector("header nav").style.left = "0";
+//     }
+//     document.querySelector("header nav > p").onclick = function(){
+//       document.querySelector("header nav").style.left = "-450px";
+//     };
+// } else if(window.innerWidth > 994){
+//     document.querySelector("header nav").style.left = "0px";
+// }
 
-  //if함수해보기
-  //header nav 100vh 말고 끝까지는?
+// $(function(){
+//   $(".topmenu").click(function(){
+//     $("header nav").css("left","0").css("transition","0.3s");
+//   });
+//   $("header nav > p").click(function(){
+//     $("header nav").css("left","-450px");
+//   });
+// });
+
+//header nav 100vh 말고 끝까지는?
