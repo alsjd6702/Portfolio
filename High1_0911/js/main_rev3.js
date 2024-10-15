f37186a276b595953aef5a75cf017ee42deb1990
min <alsjd6702@gmail.com>
2024-10-15 12:30:15 +0900

    first commit

--- High1_0911/js/main_rev3.js
+++ High1_0911/js/main_rev3.js
@@ -6,23 +6,39 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
 
-  $(function(){
-    $(".topmenu").click(function(){
-      $("header nav").css("left","0").css("transition","0.3s");
-    });
-    $("header nav > p").click(function(e){
-      e.preventDefault();
-      $("header nav").css("left","-450px");
-    });
-  });
+let width = window.innerWidth;
+console.log(width);
+
+if(width <= 994){
+    document.querySelector(".topmenu").onclick = function(){
+        document.querySelector("header nav").style.left = "0";
+    }
+
+    document.querySelector("header nav > p").onclick = function(){
+      document.querySelector("header nav").style.left = "-450px";
+    };
+} else if(width > 994){
+    document.querySelector("header nav").style.left = "0px";
+}
+
+  // $(function(){
+  //   $(".topmenu").click(function(){
+  //     $("header nav").css("left","0").css("transition","0.3s");
+  //   });
+  //   $("header nav > p").click(function(e){
+  //     e.preventDefault();
+  //     $("header nav").css("left","-450px");
+  //   });
+  // });
+
   //if함수해보기
   //header nav 100vh 말고 끝까지는?
