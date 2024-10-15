79819ecdb45bf42eaa0f6a4ab30347987202f263
min <alsjd6702@gmail.com>
2024-10-15 15:56:17 +0900

    first commit

---
+++ LeLabo_0910/js/main_basis.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+});
